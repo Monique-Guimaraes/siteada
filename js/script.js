ae2fedb28d341355faeb017e8e02563e8fcf0768
moniqueguima <monique.pguimaraes@gmail.com>
2021-02-10 14:57:58 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("BotaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario(){
+  if(document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != "")  {
+    alert("Prontinho! Você receberá as novidades por email")
+  }else{
+    alert("Por favor, preencha todos os campos!")
+    }
+
+}
+
+
